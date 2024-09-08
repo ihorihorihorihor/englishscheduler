d6155a741da381a61df323cde94a70004e6316cd
Ihor <30538039+ihorihorihorihor@users.noreply.github.com>
2024-09-08 12:11:36 +0300

    *added export to csv file

--- scheduler.js
+++ scheduler.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
-
+    let schedule = [];
+            
     let grammar = [
-        'Section 4: Verb Tenses: green',
         'Section 5: Verb Tenses: Simple Present Verb Tense:green',
         'Section 6: Verb Tenses: Verb Tenses:Verb Conjugation (1st/2nd/3rd person):green',
         'Section 7: Verb Tenses: Subject-Verb Agreement:green',
@@ -19,18 +19,15 @@ $(document).ready(function () {
         'Section 19: Verb Tenses: Present Perfect Continuous Tense:red',
         'Section 20: Verb Tenses: Past Perfect Continuous Tense:red',
         'Section 21: Verb Tenses: Future Perfect Continuous Tense:red',
-        'Section 22: Auxiliary/Modal Verbs:green',
         'Section 23: Auxiliary/Modal Verbs:To Be:green',
         'Section 24: Auxiliary/Modal Verbs:To Do, To Have:green',
         'Section 25: Auxiliary/Modal Verbs:Can, Could:green',
         'Section 26: Auxiliary/Modal Verbs:Will, Would:blue',
         'Section 27: Auxiliary/Modal Verbs:Shall, Should:blue',
         'Section 28: Auxiliary/Modal Verbs:Must, May, Might:blue',
-        'Section 29: Phrasal Verbs:green',
         'Section 30: Phrasal Verbs:Carry On, Put Off:red',
         'Section 31: Phrasal Verbs:Turn Down, Break Up:red',
         'Section 32: Phrasal Verbs:Give up, Turn Out:red',
-        'Section 33: Nouns/Pronouns:green',
         'Section 34: Nouns/Pronouns:Common Nouns:green',
         'Section 35: Nouns/Pronouns:Proper Nouns:green',
         'Section 36: Nouns/Pronouns:Subject Pronouns:green',
@@ -42,10 +39,8 @@ $(document).ready(function () {
         'Section 42: Nouns/Pronouns:Material Nouns and Compound Nouns:blue',
         'Section 43: Nouns/Pronouns:Countable vs. Uncountable Nouns:red',
         'Section 44: Nouns/Pronouns:Collective Nouns:red',
-        'Section 45: Articles/Demonstratives:green',
         'Section 46: Articles/Demonstratives:Articles (a, an, the):green',
         'Section 47: Articles/Demonstratives:This, That, These, Those:green',
-        'Section 48: Adjectives:green',
         'Section 49: Adjectives:Adjectives and Adverbs:green',
         'Section 50: Adjectives:Descriptives Adjectives:green',
         'Section 51: Adjectives:Proper Adjectives:green',
@@ -54,7 +49,6 @@ $(document).ready(function () {
         'Section 54: Adjectives:Superlative Adjectives:blue',
         'Section 55: Adjectives:Limiting Adjectives:red',
         'Section 56: Adjectives:Pronominal Adjectives:red',
-        'Section 57: Adverbs:green',
         'Section 58: Adverbs:Forming Adverbs:green',
         'Section 59: Adverbs:Adverbs of Manner:blue',
         'Section 60: Adverbs:Adverbs of Frequency:blue',
@@ -72,39 +66,32 @@ $(document).ready(function () {
         'Section 72: Distributives/Quantifiers/Pre-determiners:Much, Many:blue',
         'Section 73: Distributives/Quantifiers/Pre-determiners:Some, Any, Enough:blue',
         'Section 74: Distributives/Quantifiers/Pre-determiners:Such, What, Rather, Quite:blue',
-        'Section 75: Preposition:green',
-        'Section 76: Preposition:Prepositions:green',
         'Section 77: Preposition:Prepositions of Time:blue',
         'Section 78: Preposition:Prepositions of Place:blue',
         'Section 79: Preposition:Prepositions of Manner:blue',
         'Section 80: Preposition:Prepositions of Direction and Motion:blue',
         'Section 81: Preposition:Prepositions of Cause,Purpose, Reason:blue',
         'Section 82: Preposition:Prepositional Phrases:blue',
-        'Section 83: Clauses:green',
         'Section 84: Clauses:Clauses:blue',
         'Section 85: Clauses:Independent Clause:blue',
         'Section 86: Clauses:Dependent (Subordinate)Clause:blue',
         'Section 87: Clauses:Noun Clause:red',
         'Section 88: Clauses:Adjective (Relative) Clause:red',
         'Section 89: Clauses:Adverb Clauses:red',
-        'Section 90: Conjunctions:green',
         'Section 91: Conjunctions:Conjunctions:green',
         'Section 92: Conjunctions:Coordinating Conjunctions:blue',
         'Section 93: Conjunctions:Subordinate Conjunctions:blue',
         'Section 94: Conjunctions:Compound Conjunctions:red',
         'Section 95: Conjunctions:Correlative Conjunctions:red',
         'Section 96: Conjunctions:Pseudo Conjunctions:red',
-        'Section 97: Conditionals:green',
         'Section 98: Conditionals:Conditionals:blue',
         'Section 99: Conditionals:Conditionals: Type 0:red',
         'Section 100: Conditionals:Conditionals: Type 1:red',
         'Section 101: Conditionals:Conditionals: Type 2:red',
         'Section 102: Conditionals:Conditionals: Type 3:red',
         'Section 103: Conditionals:Conditionals: Type Mixed:red',
-        'Section 104: Stand Alone Topics:green',
         'Section 105: Stand Alone Topics:Passive vs. Active Voice:red',
         'Section 106: Stand Alone Topics:Reported Speech:red',
-        'Section 107: Common Mistakes:green',
         'Section 108: Common Mistakes:Fewer vs. Less | Then vs. Than:red',
         'Section 109: Common Mistakes:There, Their, They&#39;re | To vs. Too vs. Two:red'];
 
@@ -201,8 +188,9 @@ $(document).ready(function () {
 
         let total = grammarcopy.length;
         let day = 1;
-        let group = $('<div class="row row-cols-1 row-cols-sm-2 row-cols-md-4"></div>');
+
         do {
+
             var punctuationtopic = punctuationcopy.pop();
             if (typeof punctuationtopic === 'undefined') {
                 punctuationcopy = punctuation.slice();
@@ -215,26 +203,93 @@ $(document).ready(function () {
                 shuffle(speakingcopy);
                 speakingtopic = speakingcopy.pop();
             }
-            var card = $('<div class="card h-100" style="width: 18rem;"></div>');
-            var content = $('<p class="card-text"></p>');
-            content.append('<h5 class="card-title">Grammar :</h5>');
-            content.append('<p>' + getGrammarTopic(grammarcopy) + '</p>');
-            content.append('<p>' + getGrammarTopic(grammarcopy) + '</p>');
-            content.append('<p>' + getGrammarTopic(grammarcopy) + '</p>');
-            content.append('<h5 class="card-title">Punctuation : </h5>');
-            content.append('<p>' + punctuationtopic + '</p>');
-            content.append('<h5 class="card-title">Speaking : </h5>');
-            content.append('<p>' + speakingtopic + '</p>');
-            card.append('<div class="card-header">Day ' + day + '</div>');
-            card.append($('<div class="card-body"></div>').append(content));
-            group.append($('<div class="col col-sm-6 col-md-3"></div>').append(card));
+            const sday = {};
+            sday.grammar1 = getGrammarTopic(grammarcopy);
+            sday.grammar2 = getGrammarTopic(grammarcopy);
+            sday.grammar3 = getGrammarTopic(grammarcopy);
+            sday.punctuation = punctuationtopic;
+            sday.speaking = speakingtopic;
+            schedule[day] = sday;
             day++;
             total = grammarcopy.length;
         } while (total > 0);
-        $('#submitSuccessMessage').append(group);
-        $('#pdfButton').show();
+        ouputSchedule(schedule);
+        console.log(schedule);
 
     });
+    
+    $('#csvButton').hide();
+
+    $('#csvButton').click(function (e) {
+        
+        e.preventDefault();
+        $('#csvButton').hide();
+        
+        const delimiter = ',';
+        const linebreak = '\r\n';
+        let text = '';
+        let row = {};
+        
+        row.TYPE = 'task';
+        row.CONTENT = '';
+        row.DESCRIPTION = '';
+        row.PRIORITY = 1;
+        row.INDENT = 1;
+        row.AUTHOR = '';
+        row.RESPONSIBLE = '';
+        row.DATE = '';
+        row.DATE_LANG = 'en';
+        row.TIMEZONE = '';
+        row.DURATION = '';
+        row.DURATION_UNIT = '';
+        
+        //Set header row
+        text += Object.keys(row).join(delimiter) + linebreak;
+        
+        // Format JS array to plain text
+        schedule.forEach(function (entry, index) {
+            
+            let date = new Date();
+            date.setDate(date.getDate() + index);
+            let year = date.toLocaleString("default", {year: "numeric"});
+            let month = date.toLocaleString("default", {month: "2-digit"});
+            let day = date.toLocaleString("default", {day: "2-digit"});
+            let formattedDate = year + "-" + month + "-" + day;
+
+            row.CONTENT = `The Master English : Day ${index}`;
+            row.DESCRIPTION = '"'
+                    + `${entry.grammar1}`
+                    + '\r\n'
+                    + `${entry.grammar2}`
+                    + '\r\n'
+                    + `${entry.grammar3} `
+                    + '\r\n'
+                    + `${entry.punctuation} `
+                    + '\r\n'
+                    + `${entry.speaking}`
+                    + '"';
+            row.DESCRIPTION = row.DESCRIPTION.replace(/,/g, ' ');
+            row.DATE = formattedDate;
+            text += Object.values(row).join(delimiter) + linebreak;
+            
+        });
+
+        // Start CSV file download.
+        downloadscv('schedule.csv', text);
+    });
+
+    function downloadscv(filename, text) {
+        var element = document.createElement('a');
+        element.setAttribute('href', 'data:text/csv;charset=utf-8,' + encodeURIComponent(text));
+        element.setAttribute('download', filename);
+
+        element.style.display = 'none';
+        document.body.appendChild(element);
+
+        element.click();
+
+        document.body.removeChild(element);
+    }
 
     $('#pdfButton').hide();
 
@@ -281,6 +336,28 @@ $(document).ready(function () {
             }
         };
     });
+
+    function ouputSchedule(array) {
+        let group = $('<div class="row row-cols-1 row-cols-sm-2 row-cols-md-4"></div>');
+        array.forEach((entry, index) => {
+            var card = $('<div class="card h-100" style="width: 18rem;"></div>');
+            var content = $('<p class="card-text"></p>');
+            content.append('<h5 class="card-title">Grammar :</h5>');
+            content.append(`<p>${entry.grammar1}</p>`);
+            content.append(`<p>${entry.grammar2}</p>`);
+            content.append(`<p>${entry.grammar3}</p>`);
+            content.append('<h5 class="card-title">Punctuation : </h5>');
+            content.append(`<p>${entry.punctuation}</p>`);
+            content.append('<h5 class="card-title">Speaking : </h5>');
+            content.append(`<p>${entry.speaking}</p>`);
+            card.append(`<div class="card-header">Day ${index}</div>`);
+            card.append($('<div class="card-body"></div>').append(content));
+            group.append($('<div class="col col-sm-6 col-md-3"></div>').append(card));
+        });
+        $('#submitSuccessMessage').append(group);
+        $('#pdfButton').show();
+        $('#csvButton').show();
+    }
 
     function getGrammarTopic(array) {
         var topic = array.pop();
